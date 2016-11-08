96871c5b0cb62739b2e7fb954ffd6746aa281b4c
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2016-11-08 19:37:39 +0100

    prevent users from accessing the home page if they are not authenticated

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,5 +1,3 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import React from 'react';
 import { render } from 'react-dom';
 import Provider from 'react-redux/lib/components/Provider';
@@ -7,49 +5,17 @@ import Router from 'react-router/lib/Router';
 import hashHistory from 'react-router/lib/hashHistory';
 import Route from 'react-router/lib/Route';
 
+import store from 'store';
 import LoginRegisterContainer from 'containers/LoginRegisterContainer.jsx';
 import UserExpensesContainer from 'containers/UserExpensesContainer.jsx';
 
-import authenticated, { initialState as initialAuthenticatedState } from 'reducers/authenticated';
-
-// get stored id and token and use it as preloaded state for the store
-const authState = {
-  id: localStorage.getItem('id'),
-  token: localStorage.getItem('token'),
-};
-
-const store = createStore(
-  combineReducers({
-    authenticated,
-  }),
-  { authenticated: Object.assign({}, initialAuthenticatedState, authState) },
-  applyMiddleware(thunk)
-);
-
-/**
- * Persist auth state to localStorage if it has changed
- *
- * @param {string} key
- * @param {string} newVal
- * @returns {boolean}
- */
-function updateAuthStateIfNeeded(key, newVal) {
-  if (authState[key] === newVal) {
-    return false;
-  }
-
-  authState[key] = newVal;
-
-  localStorage.setItem(key, newVal);
-
-  return true;
-}
-
-store.subscribe(() => {
+hashHistory.listen(location => {
   const { id, token } = store.getState().authenticated;
-  const authData = { id, token };
 
-  Object.keys(authData).forEach(key => updateAuthStateIfNeeded(key, authData[key]));
+  // send users to login page if they are not authenticated
+  if (location.pathname === '/' && (!id || !token)) {
+    hashHistory.push('/login');
+  }
 });
 
 render(
