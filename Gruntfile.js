3b036b8ba33d4e0ba91beab6c6e51ea96b2c848e
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2016-11-09 16:35:04 +0100

    added datepicker css

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,10 +20,22 @@ module.exports = function (grunt) {
         },
       },
     },
+    cssmin: {
+      options: {
+        report: 'min',
+        keepSpecialComments: 0,
+      },
+      default: {
+        files: {
+          'static/style.min.css': 'node_modules/react-datepicker/dist/react-datepicker.css',
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', ['dev']);
 
