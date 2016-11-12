245d56641994a02e2627c11df6dd4c56d77b6e94
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2016-11-12 18:56:55 +0100

    added excludeempty option to objToQueryString func

--- src/shared/utils/objToQueryString.js
+++ src/shared/utils/objToQueryString.js
@@ -1,10 +1,24 @@
 /**
- * Convert an object to a query string so it can be sent along with a POST request
+ * Convert an object to a query string so it can be sent along with an HTTP request
  *
  * @param {object} [obj={}]
+ * @param {boolean} [excludeEmpty=false] - Set to TRUE in order to exclude from the resulting string
+ * key-value pairs with an empty value
  * @returns {string}
  */
-function objToQueryString(obj = {}) {
+function objToQueryString(obj = {}, excludeEmpty = false) {
+  if (excludeEmpty) {
+    obj = Object.keys(obj).reduce((result, key) => {
+      if (!obj[key]) {
+        return result;
+      }
+
+      result[key] = obj[key];
+
+      return result;
+    }, {});
+  }
+
   return Object.keys(obj).map(key =>
     encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])
   ).join('&');
