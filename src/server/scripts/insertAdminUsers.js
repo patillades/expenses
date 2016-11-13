f845749d1dc4faa892c8d5e4d2c53d163acc0797
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2016-11-13 21:42:25 +0100

    added script to insert admin users

---
+++ src/server/scripts/insertAdminUsers.js
@@ -0,0 +1,50 @@
+// add this file's directory to the node modules search path
+require('app-module-path').addPath(__dirname + '/..');
+
+const mongoose = require('mongoose');
+const config = require('config');
+const expect = require('expect');
+
+const usersModel = require('models/users.model');
+
+// Use native promises
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost/${config.get('db')}`);
+
+mongoose.connection.on('error', err => console.error('MongoDB error: %s', err));
+
+const db = mongoose.connection;
+db.on('error', e => {
+  console.log('db error', e);
+
+  process.exit();
+});
+
+describe('db setup', function () {
+  before(done => db.once('open', done));
+
+  it('should insert two users or fail if already registered', done => {
+    Promise.all([
+      usersModel.create(
+        config.get('admin'),
+        usersModel.ROLES.ADMIN
+      ),
+      usersModel.create(
+        config.get('user_manager'),
+        usersModel.ROLES.USER_MANAGER
+      ),
+    ]).then(
+      results => {
+        expect(results.length).toBe(2);
+
+        done();
+      },
+
+      err => {
+        expect(err).toBe('mail already registered');
+
+        done();
+      }
+    );
+  });
+});
