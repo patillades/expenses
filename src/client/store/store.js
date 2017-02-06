831665b22a2df4c8f65ff570d5f646e1b6396870
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2017-02-06 11:57:03 +0100

    save user's id to local storage

--- src/client/store/store.js
+++ src/client/store/store.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import jwtDecode from 'jwt-decode';
+import merge from 'lodash/merge';
 
 import authenticated, { initialState as initialAuthenticatedState } from 'reducers/authenticated';
 import expenses from 'reducers/expenses';
@@ -8,11 +10,28 @@ import filters from 'reducers/filters';
 import expensesView from 'reducers/expensesView';
 import modals from 'reducers/modals';
 
-// get stored token and use it as preloaded state for the store
-let storedToken = localStorage.getItem('token');
+// get data on localStorage and use it as preloaded state for redux's store
+const storedVars = {
+  id: localStorage.getItem('id'),
+  token: localStorage.getItem('token'),
+};
 
+if (storedVars.token) {
+  try {
+    jwtDecode(storedVars.token);
+  } catch (e) {
+    // remove stored data if token is falsy
+    storedVars.id = null;
+    storedVars.token = null;
+    localStorage.removeItem('id');
+    localStorage.removeItem('token');
+  }
+}
+
+// @todo remove code on production
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const { id, token } = storedVars;
 const store = createStore(
   combineReducers({
     authenticated,
@@ -22,34 +41,36 @@ const store = createStore(
     expensesView,
     modals,
   }),
-  { authenticated: Object.assign({}, initialAuthenticatedState, { token: storedToken }) },
+  { authenticated: merge({}, initialAuthenticatedState, { id, token }) },
   composeEnhancers(applyMiddleware(thunk))
 );
 
 /**
- * Persist auth state to localStorage if it has changed
+ * Persist a piece of the application state (state[statePart][stateVarName]) to localStorage
  *
- * @param {object} authState
- * @returns {boolean}
+ * @param {string} stateVarName
+ * @param {string} statePart
+ * @return {boolean}
  */
-function shouldUpdateStoredToken(authState) {
-  const { token } = authState;
+function shouldUpdateStoredState(stateVarName, statePart) {
+  const stateVar = store.getState()[statePart][stateVarName];
 
-  if (storedToken === token) {
+  if (storedVars[stateVarName] === stateVar) {
     return false;
   }
 
-  storedToken = token;
+  storedVars[stateVarName] = stateVar;
 
-  if (!token) {
-    localStorage.removeItem('token');
+  if (!stateVar) {
+    localStorage.removeItem(stateVarName);
   } else {
-    localStorage.setItem('token', token);
+    localStorage.setItem(stateVarName, stateVar);
   }
 
   return true;
 }
 
-store.subscribe(() => shouldUpdateStoredToken(store.getState().authenticated));
+store.subscribe(() => shouldUpdateStoredState('id', 'authenticated'));
+store.subscribe(() => shouldUpdateStoredState('token', 'authenticated'));
 
 export default store;
