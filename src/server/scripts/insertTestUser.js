794c404668305c7efb755d3ae42bf26b93d68fe2
Xavier Lozano Carreras <xavier.lozano.carreras@gmail.com>
2017-08-07 18:32:49 +0200

    added expense cateogries to test user

--- src/server/scripts/insertTestUser.js
+++ src/server/scripts/insertTestUser.js
@@ -6,13 +6,15 @@ const moment = require('moment');
 const config = require('config');
 const winston = require('winston');
 const expect = require('expect');
+const _ = require('lodash');
 
 const userModel = require('models/user.model');
 const expenseModel = require('models/expense.model');
+const expenseCategoryModel = require('models/expenseCategory.model');
 
 // Use native promises
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost/${config.get('db')}`);
+mongoose.connect(`mongodb://localhost/${config.get('db')}`, { useMongoClient: true });
 
 mongoose.connection.on('error', err => winston.error('MongoDB error: %s', err));
 
@@ -22,6 +24,11 @@ db.on('error', (e) => {
 
   process.exit();
 });
+
+const categories = [
+  'test',
+  'random',
+];
 
 const expenses = [
   { description: 'supermarket', amount: 15, date: moment(), comment: 'good stuff' },
@@ -36,28 +43,48 @@ const expenses = [
   { description: 'subway ticket', amount: 2, date: moment().subtract(10, 'd') },
 ];
 
-describe('admin users setup', () => {
+describe('Test user setup', () => {
   before(done => db.once('open', done));
 
   it('should insert a test user or fail if already registered', (done) => {
-    userModel.create(
-      config.get('test_user'),
-      userModel.ROLES.ADMIN
-    ).then(
-      user => Promise.all(
-        expenses.map(expense => expenseModel.create(expense, user.id))
-      ),
+    let userId;
 
-      err => Promise.reject(err)
+    userModel.create(
+      config.get('test_user')
+    ).then(
+      (user) => {
+        userId = user.id;
+
+        return true;
+      }
+    ).then(
+      () => Promise.all(
+        categories.map(title => expenseCategoryModel.create({ title }, userId))
+      )
+    ).then(
+      (categories) => Promise.all(
+        expenses.map((expense, i) => {
+          const exp = _.merge(
+            { expenseCategoryId: categories[i % 2].id },
+            expense
+          );
+
+          return expenseModel.create(exp, userId);
+        })
+      )
     ).then(
       (result) => {
         expect(result.length).toBe(expenses.length);
 
-        done();
-      },
+        console.info('DONE');
 
+        done();
+      }
+    ).catch(
       (err) => {
         expect(err).toBe('mail already registered');
+
+        console.error('ERROR:', err);
 
         done();
       }
